96a21058f6a0fb6fde267d63b6052e9e3d52e7b0
kakimochi12 <123672973+kakimochi12@users.noreply.github.com>
2026-04-12 11:32:04 +0900

    Add: longest-increasing-path-in-matrix - submission-33

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-33.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        indegree = [[0] * COLS for i in range(ROWS)]
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                for dr, dc in dirs:
+                    nr, nc = r + dr, c + dc
+                    if (nr in range(ROWS) and nc in range(COLS) and matrix[nr][nc] < matrix[r][c]):
+                        indegree[r][c] += 1
+        
+
+        q = deque()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if indegree[r][c] == 0:
+                    q.append((r, c))
+
+        res = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in dirs:
+                    nr, nc = r + dr, c + dc
+                    if (nr in range(ROWS) and nc in range(COLS) and matrix[nr][nc] > matrix[r][c]):
+                        indegree[nr][nc] -= 1
+                        if indegree[nr][nc] == 0:
+                            q.append((nr, nc))
+            res += 1
+        return res
