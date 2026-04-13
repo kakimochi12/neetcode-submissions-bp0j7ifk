972649bd63355fbde1b24569ffb4576f09892e8a
kakimochi12 <123672973+kakimochi12@users.noreply.github.com>
2026-04-14 08:57:18 +0900

    Add: count-connected-components - submission-22

---
+++ Data Structures & Algorithms/count-connected-components/submission-22.py
@@ -0,0 +1,29 @@
+class DSU:
+    def __init__(self, n):
+        self.Parent = list(range(n))
+        self.rank = [0] * n
+    def find(self, node):
+        if node != self.Parent[node]:
+            self.Parent[node] = self.find(self.Parent[node])
+        return self.Parent[node]
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv:
+            return False
+
+        if self.rank[pu] >= self.rank[pv]:
+            self.Parent[pv] = pu
+            self.rank[pu] += self.rank[pv]
+        else:
+            self.Parent[pu] = pv
+            self.rank[pv] += self.rank[pu]
+        return True
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = n
+
+        for u, v in edges:
+            if dsu.union(u, v):
+                res -= 1
+        return res
