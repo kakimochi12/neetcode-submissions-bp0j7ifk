9509a95a4abcbcfc5b29acd425228343ebe899c6
kaikiikeda <123672973+kakimochi12@users.noreply.github.com>
2026-04-25 10:02:00 +0900

    Add: valid-tree - submission-39

---
+++ Data Structures & Algorithms/valid-tree/submission-39.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = [[] for i in range(n)]
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        visited = set()
+        def dfs(node, par):
+            if node in visited:
+                return False
+                
+            visited.add(node)
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                if not dfs(nei, node):
+                    return False
+            return True
+        return dfs(0, -1) and len(visited) == n
