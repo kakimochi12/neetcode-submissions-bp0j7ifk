df4aa622b290ea53b146dde55de4278595bc4f52
kaikiikeda <123672973+kakimochi12@users.noreply.github.com>
2026-06-04 08:31:43 +0900

    Add: coin-change - submission-150

---
+++ Data Structures & Algorithms/coin-change/submission-150.py
@@ -0,0 +1,18 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+
+        def dfs(a):
+            if a in memo:
+                return memo[a]
+            if a == 0:
+                return 0
+            
+            res = float("inf")
+            for c in coins:
+                if a - c >= 0:
+                    res = min(res, 1 + dfs(a-c))
+            memo[a] = res
+            return res
+        return dfs(amount) if dfs(amount) != float("inf") else -1
+        
