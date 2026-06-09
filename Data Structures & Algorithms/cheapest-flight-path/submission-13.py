97fd0e9d15abe11d4bacf1b141c62da763283c8b
kaikiikeda <123672973+kakimochi12@users.noreply.github.com>
2026-06-10 08:57:59 +0900

    Add: cheapest-flight-path - submission-13

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-13.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float("inf")] * n
+        prices[src] = 0
+        adj = [[] for i in range(n)]
+
+        for u, v, cost in flights:
+            adj[u].append((v, cost))
+        
+        q = deque([(0, src, 0)])
+        while q:
+            cost, place, flights = q.popleft()
+            if flights > k:
+                continue
+            for nei, w in adj[place]:
+                newCost = cost + w
+                if newCost < prices[nei]:
+                    prices[nei] = newCost
+                    q.append((newCost, nei, flights + 1))
+        return prices[dst] if prices[dst] != float("inf") else -1
