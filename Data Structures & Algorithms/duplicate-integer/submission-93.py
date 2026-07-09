9813c222a62adb6999d818bf86b740964793a541
kaikiikeda <123672973+kakimochi12@users.noreply.github.com>
2026-07-09 09:36:45 +0900

    Add: duplicate-integer - submission-93

---
+++ Data Structures & Algorithms/duplicate-integer/submission-93.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        prevMap = set()
+
+        for n in nums:
+            if n in prevMap:
+                return True
+            prevMap.add(n)
+        return False
